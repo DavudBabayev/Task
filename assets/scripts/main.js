4b7d110b891257bb69cba8691566f6e9ae07e027
DavudBaba00 <davudbaba00@gmail.com>
2023-12-18 17:25:47 +0400

    Commited

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,49 @@
 let url = "http://localhost:3000/card";
 
-async function cards(){
+let card = document.querySelector(".card");
+let searchInp = document.querySelector("#search");
+let yaddashArr = [];
+let filterArr = [];
+let mu = 3;
+let loadBtn = document.querySelector(".load");
+
+
+async function getAllCards(){
     let res = await axios.get(url);
     let data = await res.data;
 
-    console.log(data);
-    
-    
+    yaddashArr = data;
+
+    card.innerHTML = "";
+    filterArr = filterArr.length || searchInp.value ? filterArr : data;
+
+    filterArr.slice(0, mu).forEach(elem => {
+        card.innerHTML += `
+        <div>
+        <a href="#">${elem.tag}</a>
+        <img src="${elem.image}" alt="">
+        <p>${elem.text}</p>
+        </div>
+        `
+    });
+
 }
 
-cards();
+getAllCards();
+
+////Load More////
+
+loadBtn.addEventListener("click", ()=>{
+    mu +=3;
+    getAllCards();
+});
+
+////Search////
+
+searchInp.addEventListener("input", (e) => {
+    filterArr = yaddashArr;
+    filterArr = filterArr.filter((element) => {
+        return element.tag.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase());
+    });
+    getAllCards();
+});
